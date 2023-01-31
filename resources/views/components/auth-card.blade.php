76162655f8081d761404aac8146c2a733260a964
pontusolsson03 <58231866+pontalainen@users.noreply.github.com>
2023-01-31 13:58:46 +0100

    a bit more contrast

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center">
-    <div class="sm:max-w-md px-6 py-4 shadow-md overflow-hidden login w-max my-auto">
+    <div class="sm:max-w-md px-6 py-4 shadow-md overflow-hidden login w-max my-auto rounded-md">
         {{ $slot }}
     </div>
 </div>
